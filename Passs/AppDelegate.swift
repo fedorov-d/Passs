3f12a4cd306ded39a16b313e68535d262eb98fd5
Dmitry Fedorov <d.a.fedorov1987@gmail.com>
2021-04-04 15:23:50 +0300

    Fix build issue, append cocoapods

--- Passs/AppDelegate.swift
+++ Passs/AppDelegate.swift
@@ -14,7 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let navigationController = UINavigationController(rootViewController: PasswordsViewController())
+        let passwordsViewController = PasswordsViewController(databaseManager: PassDatabaseManager(databaseURL: URL(string: "")!, password: ""), pasteboardManager: pasteboardManager)
+        let navigationController = UINavigationController(rootViewController: passwordsViewController)
         window = UIWindow()
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
